297df84e29b37b1908e4e160be00dd97f03734e6
jasimuddin123 <jasimuddindxn@gmail.com>
2020-05-31 06:46:43 -0700

    25-2 React Router, Install React Router Dom

--- src/App.js
+++ src/App.js
@@ -4,6 +4,13 @@ import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
   return (
     <div>
