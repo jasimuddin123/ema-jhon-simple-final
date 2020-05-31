14d39d084f00ff2b320d8290f00a575e7cc977c1
jasimuddin123 <jasimuddindxn@gmail.com>
2020-05-31 09:56:22 -0700

    25-6 Different ways to handle state, Make button navigate to a route

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
@@ -35,6 +36,8 @@ const Cart = (props) => {
             <p><small> Shipping Cost :{shipping}</small></p>
             <p><small>Tax + Vat:{tax}</small></p>
             <p>Total Price:{grandTotal}</p>
+            <br/>
+            <Link to="/review"><button className="main-button"> Rivew Order </button></Link>
 
         </div>
     );
